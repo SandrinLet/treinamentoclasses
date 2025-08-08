b649a1bc0594f0dc39c07ab7bd9080fec0596015
@Sandrinlet <leticiasandrin@gmail.com>
2025-08-08 20:38:44 -0300

    Primeiro commit

---
+++ Carro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinamentoClasses
+{
+    internal class Carro
+    {
+        public string Marca;
+        public string Modelo;
+        public int Ano;
+        public string Cor;
+        public double VelocidadeAtual;
+
+
+        public void Acelerar()
+
+
+        {
+            while (VelocidadeAtual <= 200)
+            {
+                VelocidadeAtual += 10;
+
+                Console.WriteLine($"Acelerando: {VelocidadeAtual} km/h");
+
+            }
+
+
+        }
+
+
+        public void Frear()
+        {
+            while (VelocidadeAtual != 0)
+            {
+
+                VelocidadeAtual -= 10;
+                Console.WriteLine($"Acelerando: {VelocidadeAtual} km/h");
+
+            }
+        }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Marca: {Marca} " +
+                $"Modelo {Modelo} " +
+                $"Ano {Ano} " +
+                $" Cor {Cor} " +
+                $" Velocidade Atual : {VelocidadeAtual} ");
+        
+}   }    }
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinamentoClasses
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Carro Brigadeirao = new Carro();
+            Brigadeirao.Marca = "Fiat";
+            Brigadeirao.Modelo = "Fiesta";
+            Brigadeirao.Ano = 2006;
+            Brigadeirao.Cor = "Preto";
+            Brigadeirao.VelocidadeAtual = 20;
+            Brigadeirao.ExibirDetalhes();
+            Brigadeirao.Acelerar();
+
+
+            Carro Tubarao = new Carro();
+            Tubarao.Marca = "Honda";
+            Tubarao.Modelo = "Civic";
+            Tubarao.Ano = 2002;
+            Tubarao.Cor = "Prata";
+            Tubarao.VelocidadeAtual = 150;
+            Tubarao.ExibirDetalhes();
+            Tubarao.Frear();
+
+
+
+
+
+
+        }
+    }
+}
